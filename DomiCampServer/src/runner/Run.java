82fd7df4b27a49adaf72cd96fc358c60bc1ccb1d
SrSan <SrSan@192.168.0.19>
2020-06-01 18:26:11 -0500

    1st commit

---
+++ DomiCampServer/src/runner/Run.java
@@ -0,0 +1,10 @@
+package runner;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		System.out.println("First Commit & Push Running...");
+		System.out.println("Second Try...");
+	}
+
+}
