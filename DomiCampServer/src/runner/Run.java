d9a0f8052362467586a8b405f6ce01654ec1710c
Andreck san <Andreck san@DESKTOP-03L9RH7>
2020-06-01 19:43:35 -0500

    gatito!!

--- DomiCampServer/src/runner/Run.java
+++ DomiCampServer/src/runner/Run.java
@@ -5,6 +5,25 @@ public class Run {
 	public static void main(String[] args) {
 		System.out.println("First Commit & Push Running...");
 		System.out.println("Second Try...");
+                System.out.println("gatito");
+                /*
+                                      _                        
+                      \`*-.                    
+                       )  _`-.                 
+                      .  : `. .                
+                      : _   '  \               
+                      ; *` _.   `*-._          
+                      `-.-'          `-.       
+                        ;       `       `.     
+                        :.       .        \    
+                        . \  .   :   .-'   .   
+                        '  `+.;  ;  '      :   
+                        :  '  |    ;       ;-. 
+                        ; '   : :`-:     _.`* ;
+               [bug] .*' /  .*' ; .*`- +'  `*' 
+                     `*-*   `*-*  `*-*'        
+                */
+                        
 	}
 
 }
